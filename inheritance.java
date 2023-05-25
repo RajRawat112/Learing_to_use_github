53d41afe1f16e16f84b5d026575c9743c28a441d
RajRawat <raj.rawat@ucalgary.ca>
2023-05-25 06:52:06 -0600

    Started learning in-heritance in java.

---
+++ inheritance.java
@@ -0,0 +1,6 @@
+
+public class inheritance {
+    public static void main(String[] args){
+        System.out.println("Started learning inheritance in java");
+    }
+}
---
+++ java2.java
@@ -0,0 +1,11 @@
+// Create a function to add two numbers
+// and return the sum of two numbers
+public class java2{
+    public static void main(String[] args){
+        int sum = add(5,6);   // Here sum is a variable that refers to add function.
+        System.out.println(sum);
+    }
+    static int add(int a, int b){
+        return a+b;
+    }
+}
